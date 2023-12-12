06f6ed3a30c2411dc06216991637807d70b67efc
Victor Silva <victorsilva160299@gmail.com>
2023-12-12 08:20:37 +0000

    done

---
+++ myFirstReact/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+i// src/App.jsx
+import './App.css';
+
+function App() {
+  return (
+    <div className="App"></div>
+  
+  );
+}
+
+export default App;
